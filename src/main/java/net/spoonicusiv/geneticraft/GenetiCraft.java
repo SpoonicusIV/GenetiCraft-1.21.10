a51236e25cc406a86caaf31a17ac70dbc2a65ced
SpoonicusIV <amoore203657@gmail.com>
2025-11-21 18:19:18 -0500

    Test commit with comment

--- src/main/java/net/spoonicusiv/geneticraft/GenetiCraft.java
+++ src/main/java/net/spoonicusiv/geneticraft/GenetiCraft.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//test commit
 public class GenetiCraft implements ModInitializer {
 	public static final String MOD_ID = "geneticraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
