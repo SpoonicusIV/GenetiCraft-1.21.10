3165ef120f83fc269d8a270bba51747ead2dd81f
SpoonicusIV <amoore203657@gmail.com>
2025-11-21 18:17:48 -0500

    Initial commit

---
+++ src/main/java/net/spoonicusiv/geneticraft/GenetiCraftClient.java
@@ -0,0 +1,10 @@
+package net.spoonicusiv.geneticraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenetiCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
